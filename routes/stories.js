082e0e708d6e5737970417754900aba7e5d01e81
alexandra okoro <alex.nnenna.okoro@gmail.com>
2021-12-28 04:48:49 +0000

    route for view all stories by user done

--- routes/stories.js
+++ routes/stories.js
@@ -118,4 +118,22 @@ router.delete('/:id', ensureAuth, async (req, res) => {
   }
 })
 
+// @desc User's stories
+// @route GET /stories/user/:userId
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({
+      user: req.params.userId,
+      status: 'public'
+    })
+    .populate('user')
+    .lean()
+
+    res.render('stories/index', { stories })
+  } catch (error) {
+    console.log(error)
+    return res.render('error/500')
+  }
+})
+
 module.exports = router;
